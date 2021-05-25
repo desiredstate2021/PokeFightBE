2b39115d78a58bbfc1a6de1796f2511741d6d0ea
Utsavi009 <utsavikathiriya@gmail.com>
2021-05-25 15:03:23 +0200

    GET route

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const express = require('express')
+const express = require('express')   //npm i express
 const app = express()
+const cors = require('cors')   //npm i cors
 const pokemonRouter = require('./routes/pokemonRouter')
+app.use(cors())  
 
 app.get('/', (req, res) => {
     res.send('Welcome to Pokedex Api')
 })
+
+app.use('/', pokemonRouter)
 
 app.listen(3000, console.log('Server is running on 3000'))
--- routes/pokemonRouter.js
+++ routes/pokemonRouter.js
@@ -1,9 +1,35 @@
 const express = require("express")
-const jsonData = require('../pokedex.json')
+const pokedex = require('../pokedex.json')
 const pokemonRouter = express.Router()
+pokemonRouter.use(express.json())
+
 
 pokemonRouter.get('/pokemon', (req, res) => {
-    res.send('pokemon')
+    res.send(JSON.stringify(pokedex))
+})
+
+pokemonRouter.get('/pokemon/:id', (req, res) => {
+    let { id } = req.params
+    const pokemon = pokedex.find(e => e.id === parseInt(id))
+    res.send(pokemon)
+})
+
+pokemonRouter.get('/pokemon/:id/:info', (req, res) => {
+    let { id, info } = req.params
+    const pokemon = pokedex.find(e => e.id === parseInt(id))
+    if(info === 'base') {
+        res.send(pokemon.base)
+    }
+    else if(info === 'type') {
+        res.send(pokemon.type)
+    }
+    else if(info === 'name') {
+        res.send(pokemon.name)
+    }
+    else {
+        res.send(pokemon)
+    }
+    
 })
 
 
